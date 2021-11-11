a365cacd42660872a9d7bba1ad185e21f89ebe65
Yash Agarwalla <48103661+yashagr911@users.noreply.github.com>
2021-11-11 09:36:45 +0100

    Create application.py

---
+++ application.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask import jsonify
+import numpy as np
+
+application = Flask(__name__)
+
+@application.route('/')
+def hello():
+        return 'Hello Yash\n'
+
+@application.route('/random/<n>')
+def randomvalues(n):
+        values = np.random.randint(0,10,int(n))
+        result = {'values' : values.tolist()}
+        return jsonify(result)
+
+if __name__ == '__main__':
+        application.run(debug=True)
